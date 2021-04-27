12241ad7f9f61d2064ec49a634f51fd177b59720
Osyfe <osyfe.contact@gmail.com>
2021-04-27 21:09:01 +0200

    fix export

--- opengl/build_desktop.cmd
+++ opengl/build_desktop.cmd
@@ -1,3 +1,3 @@
 cargo run --release --bin opengl_desktop
-copy /y "..\target\release\opengl_desktop.exe" "export\opengl.exe"
+copy /y "..\target\release\opengl_desktop.exe" "export\desktop\opengl.exe"
 pause
