2d22824e00e47bb7c7c8845c88ee792522a33db8
Osyfe <osyfe.contact@gmail.com>
2021-05-12 15:48:32 +0200

    cleaned gru-opengl-demo

--- gru-opengl-demo/src/prog/mod.rs
+++ gru-opengl-demo/src/prog/mod.rs
@@ -1,21 +1,26 @@
 use std::collections::{HashSet, HashMap};
-use gru_opengl::{App, Context, gl::*, event};
+use gru_opengl::{log, App, Context, gl::*, event};
 use gru_math::*;
 use gru_text::*;
+use gru_util::*;
 use rodio::{Decoder, OutputStream, OutputStreamHandle, source::{Source}, buffer::SamplesBuffer};
 
 mod text;
 mod cube;
-mod slice;
 use text::*;
 use cube::*;
-use slice::*;
 
 const FRAMEBUFFER_SIZE: u32 = 1024;
 const TARGET_ROT: Vec3 = Vec3(0.5, 0.5, 0.5);
 const ACC: f32 = 0.003;
 const WEH_VEL: f32 = 10.0;
 const SOUND_COOLDOWN: f32 = 0.5;
+
+struct AtlasData
+{
+    atlas: Atlas,
+    texture: Texture
+}
 
 struct TextData
 {
@@ -37,12 +42,6 @@ struct CubeData
     tex_key: UniformKey
 }
 
-struct AtlasData
-{
-    atlas: Atlas,
-    texture: Texture
-}
-
 struct InputData
 {
     last_pos: (f32, f32),
@@ -51,8 +50,8 @@ struct InputData
 
 struct SoundData
 {
-    device: Option<(OutputStream, OutputStreamHandle)>, //web need to waite for input
-    map: HashMap<String, (u16, u32, Vec<f32>)>,
+    device: Option<(OutputStream, OutputStreamHandle)>, //on web we need to wait for input
+    map: HashMap<String, (u16, u32, Vec<f32>)>, //name -> (channels, sample_rate, data)
     cooldown_eh: f32,
     cooldown_weh: f32
 }
@@ -60,9 +59,9 @@ struct SoundData
 pub struct Demo
 {
     run_id: u64,
+    atlas: AtlasData,
     text: TextData,
     cube: CubeData,
-    atlas: AtlasData,
     framebuffer: Framebuffer,
     rot: Rotor,
     vel: Vec3,
@@ -70,27 +69,79 @@ pub struct Demo
     sound: SoundData
 }
 
+impl Demo
+{
+    fn init_audio(&mut self)
+    {
+        if self.sound.device.is_none()
+        {
+            log("init audio");
+            self.sound.device = Some(OutputStream::try_default().unwrap());
+        }
+    }
+
+    fn play_audio(&mut self, name: &str)
+    {
+        if let Some((channels, sample_rate, data)) = self.sound.map.get(name)
+        {
+            if let Some(device) = &self.sound.device
+            {
+                let buffer = SamplesBuffer::new(*channels, *sample_rate, data.clone());
+                device.1.play_raw(buffer).unwrap();
+            }
+        }
+    }
+
+    fn play_eh(&mut self)
+    {
+        if self.sound.cooldown_eh <= 0.0
+        {
+            self.sound.cooldown_eh = SOUND_COOLDOWN;
+            self.play_audio("eh.ogg");
+        }
+    }
+
+    fn play_weh(&mut self)
+    {
+        if self.sound.cooldown_weh <= 0.0
+        {
+            self.sound.cooldown_weh = SOUND_COOLDOWN;
+            self.play_audio("weh.ogg");
+        }
+    }
+}
+
 impl App for Demo
 {
 	fn init(ctx: &mut Context) -> Self
 	{
+        //read storage
         let run_id = match ctx.storage.get("ID")
         {
             Some(id) => id.parse().unwrap(),
             None => 0
         };
-        gru_opengl::log(&format!("run_id = {}", run_id));
-
+        log(&format!("run_id = {}", run_id));
+        //load files
+        ctx.load_file("eh.ogg");
+        ctx.load_file("weh.ogg");
+        //graphic
         let gl = &mut ctx.gl;
-
+        //font
+        let mut alphabet = HashSet::new();
+        for text in &TEXTS { for c in text.chars() { if c != ' ' { alphabet.insert(c); } } }
+        let font = Font::new(include_bytes!("../res/futuram.ttf"));
+        let (font_texture, atlas) = Atlas::new(&font, &alphabet, 100.0, ATLAS_SIZE, 5);
+        if font_texture.len() > 1 { panic!("Atlas more than one page"); }
+        let atlas_texture = gl.new_texture(&TextureConfig { size: ATLAS_SIZE, channel: TextureChannel::A, mipmap: false, wrap: TextureWrap::Repeat}, &font_texture[0]);
+        //text
         let max_chars = TEXTS.iter().map(|text| text.chars().count() as u32).max().unwrap();
-
         let text_shader = gl.new_shader(include_str!("../glsl/text.vert"), include_str!("../glsl/text.frag"));
         let text_vertices = gl.new_vertex_buffer(max_chars * 4, BufferAccess::DYNAMIC);
         let text_indices = gl.new_index_buffer(max_chars * 6, BufferAccess::DYNAMIC);
         let height_key = text_shader.get_key("height").unwrap().clone();
         let atlas_key = text_shader.get_key("atlas").unwrap().clone();
-
+        //cube
         let cube_shader = gl.new_shader(include_str!("../glsl/cube.vert"), include_str!("../glsl/cube.frag"));
         let (indices, vertices) = cube();
         let mut cube_vertices = gl.new_vertex_buffer(vertices.len() as u32, BufferAccess::STATIC);
@@ -99,36 +150,17 @@ impl App for Demo
         cube_indices.data(0, &indices);   
         let mat_key = cube_shader.get_key("mat").unwrap().clone();
         let tex_key = cube_shader.get_key("tex").unwrap().clone();
-
-        let mut alphabet = HashSet::new();
-        for text in &TEXTS { for c in text.chars() { if c != ' ' { alphabet.insert(c); } } }
-        let font = Font::new(include_bytes!("../res/futuram.ttf"));
-        let (font_texture, atlas) = Atlas::new(&font, &alphabet, 100.0, ATLAS_SIZE, 5);
-        if font_texture.len() > 1 { panic!("Atlas more than one page"); }
-        let atlas_texture = gl.new_texture(&TextureConfig { size: ATLAS_SIZE, channel: TextureChannel::A, mipmap: false, wrap: TextureWrap::Repeat}, &font_texture[0]);
-
+        //framebuffer
         let framebuffer = gl.new_framebuffer(&FramebufferConfig { depth: false, size: FRAMEBUFFER_SIZE, wrap: TextureWrap::Clamp });
-
-        let input = InputData
-        {
-            last_pos: (0.0, 0.0),
-            mouse_down: false
-        };
-
-        let sound = SoundData
-        {
-            device: None,
-            map: HashMap::new(),
-            cooldown_eh: 0.0,
-            cooldown_weh: 0.0
-        };
-
-        ctx.load_file("eh.ogg");
-        ctx.load_file("weh.ogg");
-
+        //pack everything
 		Self
         {
             run_id,
+            atlas: AtlasData
+            {
+                atlas,
+                texture: atlas_texture
+            },
             text: TextData
             {
                 shader: text_shader,
@@ -142,47 +174,42 @@ impl App for Demo
                 vertices: cube_vertices, indices: cube_indices,
                 mat_key, tex_key
             },
-            atlas: AtlasData
-            {
-                atlas,
-                texture: atlas_texture
-            },
             framebuffer,
             rot: Rotor::identity(),
             vel: TARGET_ROT,
-            input,
-            sound
+            input: InputData
+            {
+                last_pos: (0.0, 0.0),
+                mouse_down: false
+            },
+            sound: SoundData
+            {
+                device: None,
+                map: HashMap::new(),
+                cooldown_eh: 0.0,
+                cooldown_weh: 0.0
+            }
         }
 	}
 
     fn input(&mut self, _ctx: &mut Context, event: event::Event)
     {
-        fn init_audio(sound: &mut SoundData)
-        {
-            if sound.device.is_none()
-            {
-                gru_opengl::log("init audio");
-                sound.device = Some(OutputStream::try_default().unwrap());
-            }
-        }
         use event::*;
-        let mut sound = None;
         match event
         {
+            Event::File(name, data) =>
+            {
+                let decoder = Decoder::new_vorbis(SliceReadSeek::new(&data)).unwrap();
+                let (channels, sample_rate) = (decoder.channels(), decoder.sample_rate());
+                let data = decoder.convert_samples::<f32>().collect::<Vec<_>>();
+                self.sound.map.insert(name, (channels, sample_rate, data));
+            },
             Event::Click { button: MouseButton::Left, state } =>
             {
-                init_audio(&mut self.sound);
+                self.init_audio();
                 self.input.mouse_down = state == ElementState::Pressed;
-                if self.input.mouse_down && self.sound.cooldown_eh <= 0.0
-                {
-                    self.sound.cooldown_eh = SOUND_COOLDOWN;
-                    sound = Some("eh.ogg");
-                }
-                if !self.input.mouse_down && self.vel.norm() > WEH_VEL && self.sound.cooldown_weh <= 0.0
-                {
-                    self.sound.cooldown_weh = SOUND_COOLDOWN;
-                    sound = Some("weh.ogg");
-                }
+                if self.input.mouse_down { self.play_eh() }
+                else if self.vel.norm() > WEH_VEL { self.play_weh(); }
             }
             Event::Cursor { position } =>
             {
@@ -201,6 +228,12 @@ impl App for Demo
                 let (x, y) = position;
                 match phase
                 {
+                    TouchPhase::Started =>
+                    {
+                        self.init_audio();
+                        self.play_eh();
+                    },
+                    TouchPhase::Ended => if self.vel.norm() > WEH_VEL { self.play_weh() },
                     TouchPhase::Moved =>
                     {
                         let (x2, y2) = self.input.last_pos;
@@ -208,53 +241,25 @@ impl App for Demo
                         let vel = ACC * diff.norm().sqrt() + ACC;
                         self.vel += diff * vel;
                     },
-                    TouchPhase::Ended => if self.vel.norm() > WEH_VEL && self.sound.cooldown_weh <= 0.0
-                    {
-                        self.sound.cooldown_weh = SOUND_COOLDOWN;
-                        sound = Some("weh.ogg");
-                    },
-                    TouchPhase::Started => if self.sound.cooldown_eh <= 0.0
-                    {
-                        init_audio(&mut self.sound);
-                        self.sound.cooldown_eh = SOUND_COOLDOWN;
-                        sound = Some("eh.ogg")
-                    },
                     TouchPhase::Cancelled => {}
                 }
                 self.input.last_pos = position;
             },
-            Event::File(name, data) =>
-            {
-                let decoder = Decoder::new_vorbis(SliceReadSeek::new(&data)).unwrap();
-                let (channels, sample_rate) = (decoder.channels(), decoder.sample_rate());
-                let data = decoder.convert_samples::<f32>().collect::<Vec<_>>();
-                self.sound.map.insert(name, (channels, sample_rate, data));
-            }
             _ => {}
-        }
-        if let Some(name) = sound
-        {
-            if let Some((channels, sample_rate, data)) = self.sound.map.get(name)
-            {
-                if let Some(device) = &self.sound.device
-                {
-                    let buffer = SamplesBuffer::new(*channels, *sample_rate, data.clone());
-                    device.1.play_raw(buffer).unwrap();
-                }
-            }
         }
     }
 
     fn frame(&mut self, ctx: &mut Context, dt: f32, window_dims: (u32, u32)) -> bool
     {
+        //cooldown
         self.sound.cooldown_eh -= dt;
         self.sound.cooldown_weh -= dt;
-
-        let gl = &mut ctx.gl;
-        //physic
+        //physi
         self.vel += (TARGET_ROT - self.vel) * dt;
         self.rot = Rotor::from_axis(self.vel * dt) * self.rot;
         self.rot.fix();
+        //graphic
+        let gl = &mut ctx.gl;
         //text
         let current_text = if self.vel.norm() > WEH_VEL { Text::Wuhu } else { Text::Hello };
         let (text, num_lines) = current_text.get();
@@ -298,6 +303,6 @@ impl App for Demo
 
     fn deinit(self, ctx: &mut Context)
     {
-        ctx.storage.set("ID", Some(&format!("{}", self.run_id + 1)));
+        ctx.storage.set("ID", Some(&format!("{}", self.run_id + 1))); //write storage
     }
 }
